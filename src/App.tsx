ef2c01b5f941595f1de85b8afeb24420c606a9fb
Ton <washingtonf.nascimento@gmail.com>
2021-03-16 08:34:13 -0400

    Configuração da estrutura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import GlobalStyles from './shared/styles/global';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Pedra, papel , tesoura, lagarto e Spock</h1>
+      <GlobalStyles />
+    </>
   );
-}
+};
 
 export default App;
